5f57671695c8d958642586d7babd5be5d3e144b0
Xelton <53794097+tripathis617@users.noreply.github.com>
2026-02-16 20:13:01 +0000

    implement count quadraplets functionality

--- 1-D arrays/countQuadraplets.java
+++ 1-D arrays/countQuadraplets.java
@@ -1,1 +1,31 @@
-// Q — Count Quadraplets
+import java.util.*;
+
+public class Array_1D {
+    public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int k = scn.nextInt();
+      int[] arr = new int[n];
+      for (int j=0; j<n; j++) {
+        arr[j] = scn.nextInt();
+      }
+      func(arr, n, k);
+    }
+    
+    public static void func(int[] arr, int n, int k) {
+      int count = 0;
+      for(int l=0; l<n; l++) {
+        for(int m=l+1; m<n; m++) {
+          for(int i=m+1; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+              if (arr[l] - 2*arr[m] + 3*arr[i] - 4*arr[j] == k) {
+                // System.out.println("idx: " + l + " " + m + " " + i + " " + j);
+                count++;
+              } 
+            }
+          }
+        }
+      }
+      System.out.println(count);
+    }
+}
