90638281cbbd69c2dacb785952a52b0b70259544
Xelton <53794097+tripathis617@users.noreply.github.com>
2026-02-19 16:55:35 +0000

    2D Array

--- 2-D arrays/boundaryTraversal.java
+++ 2-D arrays/boundaryTraversal.java
@@ -1,1 +1,50 @@
-// J — Boundary Traversal
+import java.io.*;
+import java.util.*;
+
+public class Array_2D {
+    public static void main(String[] args) throws IOException {
+        FastScanner sc = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+          System.out.print(arr[0][i] + " "); 
+        }
+        for (int i = 1; i < n; i++) {
+          System.out.print(arr[i][m-1] + " "); 
+        }
+        if (n!=1) {
+          for (int i = m-2; i >= 0; i--) {
+            System.out.print(arr[n-1][i] + " "); 
+          }  
+        }
+        if (m!=1) {
+          for (int i = n-2; i >= 1; i--) {
+            System.out.print(arr[i][0] + " "); 
+          } 
+        }
+        out.flush();
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try { st = new StringTokenizer(br.readLine()); } 
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+    }
+}
--- 2-D arrays/waveI.java
+++ 2-D arrays/waveI.java
@@ -1,1 +1,45 @@
-// H — Wave - I
+import java.io.*;
+import java.util.*;
+
+public class WaveI {
+    public static void main(String[] args) throws IOException {
+        FastScanner sc = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+          if (i%2==0) {
+            for (int j = 0; j < m; j++) {
+              System.out.print(arr[i][j] + " ");
+            } 
+          } else {
+            for (int j = m-1; j >=0; j--) {
+              System.out.print(arr[i][j] + " ");
+            } 
+          }
+        }
+        out.flush();
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try { st = new StringTokenizer(br.readLine()); } 
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+    }
+}
